86139983e967103808f62d1a5ae54970e47d48db
Yggdrasil-dono <kaiserehrlich@web.de>
2015-09-20 20:32:56 +0200

    Changed core.js

--- locale/core.js
+++ locale/core.js
@@ -1,55 +1,62 @@
 app.translator.plural = function(count) {
-  if (count >= 0 && count < 2) {
-    return 'one';
-    }
-  else {
-    return 'other';
-  }
+  return count == 1 ? 'one' : 'other';
 };
 
-moment.locale('fr', {
-  months : 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
-  monthsShort : 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
-  weekdays : 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
-  weekdaysShort : 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
-  weekdaysMin : 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
+function processRelativeTime(number, withoutSuffix, key, isFuture) {
+  var format = {
+    'm': ['eine Minute', 'einer Minute'],
+    'h': ['eine Stunde', 'einer Stunde'],
+    'd': ['ein Tag', 'einem Tag'],
+    'dd': [number + ' Tage', number + ' Tagen'],
+    'M': ['ein Monat', 'einem Monat'],
+    'MM': [number + ' Monate', number + ' Monaten'],
+    'y': ['ein Jahr', 'einem Jahr'],
+    'yy': [number + ' Jahre', number + ' Jahren']
+  };
+  return withoutSuffix ? format[key][0] : format[key][1];
+};
+
+moment.locale('de', {
+  months : 'Januar_Februar_März_April_Mai_Juni_Juli_August_September_Oktober_November_Dezember'.split('_'),
+  monthsShort : 'Jan._Febr._Mrz._Apr._Mai_Jun._Jul._Aug._Sept._Okt._Nov._Dez.'.split('_'),
+  weekdays : 'Sonntag_Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag'.split('_'),
+  weekdaysShort : 'So._Mo._Di._Mi._Do._Fr._Sa.'.split('_'),
+  weekdaysMin : 'So_Mo_Di_Mi_Do_Fr_Sa'.split('_'),
   longDateFormat : {
-    LT : 'HH:mm',
-    LTS : 'HH:mm:ss',
-    L : 'DD/MM/YYYY',
-    LL : 'D MMMM YYYY',
-    LLL : 'D MMMM YYYY HH:mm',
-    LLLL : 'dddd D MMMM YYYY HH:mm'
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L : 'DD.MM.YYYY',
+    LL : 'D. MMMM YYYY',
+    LLL : 'D. MMMM YYYY HH:mm',
+    LLLL : 'dddd, D. MMMM YYYY HH:mm'
   },
   calendar : {
-    sameDay: '[Aujourd\'hui à] LT',
-    nextDay: '[Demain à] LT',
-    nextWeek: 'dddd [à] LT',
-    lastDay: '[Hier à] LT',
-    lastWeek: 'dddd [dernier à] LT',
-    sameElse: 'L'
+    sameDay: '[Heute um] LT [Uhr]',
+    sameElse: 'L',
+    nextDay: '[Morgen um] LT [Uhr]',
+    nextWeek: 'dddd [um] LT [Uhr]',
+    lastDay: '[Gestern um] LT [Uhr]',
+    lastWeek: '[letzten] dddd [um] LT [Uhr]'
   },
   relativeTime : {
-    future : 'dans %s',
-    past : 'il y a %s',
-    s : 'quelques secondes',
-    m : 'une minute',
-    mm : '%d minutes',
-    h : 'une heure',
-    hh : '%d heures',
-    d : 'un jour',
-    dd : '%d jours',
-    M : 'un mois',
-    MM : '%d mois',
-    y : 'un an',
-    yy : '%d ans'
+    future : 'in %s',
+    past : 'vor %s',
+    s : 'ein paar Sekunden',
+    m : processRelativeTime,
+    mm : '%d Minuten',
+    h : processRelativeTime,
+    hh : '%d Stunden',
+    d : processRelativeTime,
+    dd : processRelativeTime,
+    M : processRelativeTime,
+    MM : processRelativeTime,
+    y : processRelativeTime,
+    yy : processRelativeTime
   },
-  ordinalParse: /\d{1,2}(er|)/,
-  ordinal : function (number) {
-    return number + (number === 1 ? 'er' : '');
-  },
+  ordinalParse: /\d{1,2}\./,
+  ordinal : '%d.',
   week : {
-    dow : 1,
-    doy : 4
+    dow : 1, // Monday is the first day of the week.
+    doy : 4  // The week that contains Jan 4th is the first week of the year.
   }
 });
